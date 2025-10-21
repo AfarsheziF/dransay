0784acd928922b7a4921babc295a424ee3c33728
Or Sarfati <afrshezif@hotmail.com>
2025-10-22 00:57:41 +0200

    Simplify Vercel config - use framework detection instead of custom runtime

--- server.js
+++
@@ -1,7 +0,0 @@
-import { createRequestHandler } from "@remix-run/node";
-import * as serverBuild from "./build/index.js";
-
-export default createRequestHandler({
-  build: serverBuild,
-  mode: process.env.NODE_ENV,
-});
