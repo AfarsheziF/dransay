3345d78640ae4e1d9b3996e493dd66e7a7bee893
Or Sarfati <afrshezif@hotmail.com>
2025-10-22 01:23:44 +0200

    again

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,13 @@ import { vercelPreset } from "@vercel/remix/vite";
 installGlobals();
 
 // Suppress CJS deprecation warning
-const originalConsoleWarn = console.warn;
-console.warn = function (...args: any[]) {
-  if (args[0]?.includes?.("The CJS build of Vite's Node API is deprecated")) {
-    return;
-  }
-  originalConsoleWarn.apply(console, args);
-};
+// const originalConsoleWarn = console.warn;
+// console.warn = function (...args: any[]) {
+//   if (args[0]?.includes?.("The CJS build of Vite's Node API is deprecated")) {
+//     return;
+//   }
+//   originalConsoleWarn.apply(console, args);
+// };
 
 export default defineConfig({
   plugins: [
