1ad92ac4771262ed692ff136a714b4812893b5d3
Or Sarfati <afrshezif@hotmail.com>
2025-10-22 01:21:38 +0200

    again

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { vercelPreset } from "@vercel/remix/vite";
 
 installGlobals();
@@ -19,5 +20,6 @@ export default defineConfig({
     remix({
       presets: [vercelPreset()],
     }),
+    tsconfigPaths(),
   ],
 });
