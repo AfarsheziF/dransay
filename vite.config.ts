5b9f67bb9925004e3c78b2a232dda5e74495e9c2
Or Sarfati <afrshezif@hotmail.com>
2025-10-22 02:32:41 +0200

    again

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,35 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { vercelPreset } from "@vercel/remix/vite";
 
-installGlobals();
+// installGlobals();
+installGlobals({ nativeFetch: true });
+
+// export default defineConfig({
+//   plugins: [
+//     remix({
+//       presets: [vercelPreset()],
+//     }),
+//     tsconfigPaths(),
+//   ],
+// });
 
 export default defineConfig({
   plugins: [
     remix({
       presets: [vercelPreset()],
+      ignoredRouteFiles: ["**/.*"],
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: true,
+        v3_routeConfig: true,
+      },
     }),
     tsconfigPaths(),
   ],
+  build: {
+    assetsInlineLimit: 0,
+  },
 });
